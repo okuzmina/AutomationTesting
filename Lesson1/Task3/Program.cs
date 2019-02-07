3fd3a70dbe72f10c764c5703f105de15fb7db500
Olga Kuzmina <olga.kuzmina@arcadia.spb.ru>
2019-02-07 10:43:12 +0300

    remove bool variables

--- Lesson1/Task3/Program.cs
+++ Lesson1/Task3/Program.cs
@@ -15,12 +15,9 @@ namespace Task3
             Console.Write("Enter third row: ");
             string row3Upper = Console.ReadLine().ToUpper();
 
-            bool a = row1Upper.StartsWith(row2Upper);
-            bool b = row1Upper.EndsWith(row3Upper);
-
             Console.WriteLine();
-            Console.WriteLine("String " + row1Upper + " starts from " + row2Upper + ": " + a);
-            Console.WriteLine("String " + row1Upper + " ends with " + row3Upper + ": " + b);
+            Console.WriteLine("String " + row1Upper + " starts from " + row2Upper + ": " + row1Upper.StartsWith(row2Upper));
+            Console.WriteLine("String " + row1Upper + " ends with " + row3Upper + ": " + row1Upper.EndsWith(row3Upper));
             Console.ReadKey();
         }
     }
