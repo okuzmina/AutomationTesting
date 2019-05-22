2fd7e9d71edd67498d6a18a01daf894d2f06428d
Olga Kuzmina <olga.kuzmina@arcadia.spb.ru>
2019-05-22 10:07:53 +0300

    Singleton pattern realisation

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            if (instance1 == instance2)
+                Console.WriteLine("You can have only one instance of the entity");
+
+            Console.ReadKey();
+        }
+    }
+}
